12f25ecaf961abbb0012a76e9fd1a7975a6cfba4
unknown <fmendoza.ospina7@outlook.com>
2023-10-15 20:40:48 -0500

    Lasts changes

--- routes/about.tsx
+++ routes/about.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layouts.tsx";
 export default function about() {
   return (
     <Layout>
-      about
+      <h1 class="text-2xl text-center">About</h1>
     </Layout>
   );
 }
